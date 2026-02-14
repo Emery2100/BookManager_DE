6934e7a0368d7fdc2c4787bf27f9da6c42d00b5b
David Emery <Emery2100@yahoo.com>
2026-02-13 18:57:01 -0800

    Initial Commit

---
+++ BookManager_DE/BookManager_DEApp.swift
@@ -0,0 +1,32 @@
+//
+//  BookManager_DEApp.swift
+//  BookManager_DE
+//
+//  Created by David Emery on 2/13/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BookManager_DEApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BookManager_DETests/BookManager_DETests.swift
@@ -0,0 +1,17 @@
+//
+//  BookManager_DETests.swift
+//  BookManager_DETests
+//
+//  Created by David Emery on 2/13/26.
+//
+
+import Testing
+@testable import BookManager_DE
+
+struct BookManager_DETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
